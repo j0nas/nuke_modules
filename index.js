ba25ec098c51555f0040ff840eaf19bb7a3010a8
Jonas Jensen <jonas.jensen@greenbird.com>
2017-06-10 17:37:19 +0200

    log deletion progress

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const path = require('path');
 const pkgDir = require('pkg-dir');
 const readDirEnhanced = require('readdir-enhanced');
 const rimraf = require('rimraf');
+const logUpdate = require('log-update');
 
 const existsAsync = path =>
     (new Promise((resolve, reject) =>
         fs.access(path, error => !error ? resolve(path) : reject(error))
     ));
 
+let nodeModulesPath;
+
 pkgDir(process.cwd())
     .then(rootDir => existsAsync(path.join(rootDir, 'node_modules')))
-    .then(existingNodeModulesPath => readDirEnhanced.async(existingNodeModulesPath).then(files => files.map(file => path.join(existingNodeModulesPath, file))))
-    .then(files => {
-        files.forEach((file, index) => {
-            rimraf.sync(file);
-            console.log("Deleting: " + index + "/" + files.length);
-        })
-    })
+    .then(existingNodeModulesPath => nodeModulesPath = existingNodeModulesPath)
+    .then(() => readDirEnhanced.async(nodeModulesPath))
+    .then(files => files.map(file => path.join(nodeModulesPath, file)))
+    .then(files => [].concat(files, nodeModulesPath))
+    .then(files => files.forEach((file, index) => {
+        logUpdate(`Deleting: ${index + 1}/${files.length}`);
+        rimraf.sync(file);
+    }))
     .catch(err => console.error("An error occurred:", err));
